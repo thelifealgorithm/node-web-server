dcce0c7231e59e21b237203987d47a81a36293d2
vagrant <vagrant@vm.localdomain>
2018-02-11 11:24:09 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+//app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    var log = `${new Date().toString()}: ${req.method} ${req.url} \n`;
+    fs.appendFile('server.log', log, err => {
+        if (err) {
+            console.log('Unable to append to log file');
+        }
+    });
+    next();
+});
+
+/*
+app.use((req, res, next) => {
+    res.render('maintenance.hbs')
+});
+*/
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', text => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    //res.send('<h1>Hello Express</h1>');
+    res.render('home.hbs', {
+        welcomeMessage: 'Welcome to my website',
+        pageTitle: 'Home Page'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on on port 3000');
+});
